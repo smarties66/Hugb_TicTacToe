a7c5f111b5725cf07a59cf958be43a815722ba38
Sverrir <sverrir1993@gmail.com>
2015-10-30 21:47:22 +0000

    TicTacToeLogic testGetGrid passed

--- src/main/java/is/ru/tictactoe/TicTacToeLogic.java
+++ src/main/java/is/ru/tictactoe/TicTacToeLogic.java
@@ -1,4 +1,28 @@
 package is.ru.tictactoe;
 
 public class TicTacToeLogic {
+
+	private TicTacToePlayer player1;
+	private TicTacToePlayer player2;
+	private int roundCount;
+	private boolean isPlayer1Turn;
+	private Character[] grid;
+
+	/******************************
+	[0][1][2]
+	[3][4][5]
+	[6][7][8]
+	*******************************/
+
+	public TicTacToeLogic(TicTacToePlayer player1, TicTacToePlayer player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+		roundCount = 0;
+		isPlayer1Turn = true;
+		grid = new Character[9];
+	}
+
+	public Character[] getGrid() {
+		return grid;
+	}
 }
--- src/test/java/is/ru/tictactoe/TicTacToeLogicTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeLogicTest.java
@@ -8,4 +8,21 @@ public class TicTacToeLogicTest {
 	public static void main(String args[]) {
 		org.junit.runner.JUnitCore.main("is.ru.tictactoe.TicTacToeLogicTest");
 	}
+
+	private TicTacToeLogic defaultLogic() {
+		return new TicTacToeLogic(new TicTacToePlayer("Player1"), new TicTacToePlayer("Player2"));
+	}
+
+	@Test
+	public void testGetGrid() {
+		Character[] expectedGrid = 
+		{ 
+			null, null, null, 
+			null, null, null, 
+			null, null, null,
+		};
+
+		TicTacToeLogic logic = defaultLogic();
+		assertArrayEquals(expectedGrid, logic.getGrid());
+	}
 }
